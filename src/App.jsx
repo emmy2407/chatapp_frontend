20df7955f78d932bdbf108ebc9be1f5d3178a666
emmy2407 <bemma2407@gmail.com>
2023-06-12 17:20:18 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Routes from "./Routes";
 
 function App() {
   axios.defaults.baseURL = "https://api-chatapp-mauve.vercel.app";
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
       <Routes />
