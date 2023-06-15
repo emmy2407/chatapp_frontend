92b5acca8493bbcae42f3021468f79de9a554db9
emmy2407 <bemma2407@gmail.com>
2023-06-15 12:10:42 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
+<<<<<<< HEAD
   axios.defaults.baseURL = "https://api-chatapp-mauve.vercel.app";
   // axios.defaults.withCredentials = true;
+=======
+  // axios.defaults.baseURL = 'http://localhost:4040/';
+  axios.defaults.baseURL = 'https://mern-chat-app-api-1tnt.onrender.com/';
+  axios.defaults.withCredentials = true;
+>>>>>>> d30124aac344f5ed3082f4c5fc9e95340c1b29f0
   return (
     <UserContextProvider>
       <Routes />
